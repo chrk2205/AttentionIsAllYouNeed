2d288d99637cd5d540997e07b5a6aba08a89bc88
rohith <rohitkumar2205@gmail.com>
2026-05-02 12:29:05 +0900

    Enhance training script by integrating Weights & Biases (wandb) for logging and tracking. Update DataLoader batch size configuration and implement early stopping callback for improved training efficiency. Add wandb as a dependency in pyproject.toml and uv.lock.

--- transformer/src/train.py
+++ transformer/src/train.py
@@ -8,9 +8,18 @@ from torch.utils.data import DataLoader
 from src.trainer import LitTrainer
 from src.model import Transformer
 import lightning as L
+from pytorch_lightning.loggers import WandbLogger
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+import wandb
+
 
 # @run_and_exit(TrainArgs, description="My Training CLI")
 def train(train_args : TrainArgs):
+
+    batch_size = 64
+
+    wandb_logger = WandbLogger(project='learn transformer ')
+    wandb_logger.experiment.config["batch_size"] = batch_size
 
     dataset = load_dataset("Helsinki-NLP/opus-100", "en-hi", split="train")
     en_tokenizer = load_train_save_bpe(tokenizer_name="en", train_data=[v['en'] for v in dataset['translation']])
@@ -25,21 +34,25 @@ def train(train_args : TrainArgs):
     validation_dataset = English2HindiDataset(
         encoder_tokenizer=en_tokenizer,
         decoder_tokenizer=hi_tokenizer,
-        split="validation"
+        split="train"
     )
 
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn, num_workers=8, persistent_workers=True)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=32, shuffle=False, collate_fn=collate_fn, num_workers=8, persistent_workers=True)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, num_workers=8, persistent_workers=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, num_workers=8, persistent_workers=True)
 
     model = Transformer(en_vocab_size=len(en_tokenizer.get_vocab()), de_vocab_size=len(hi_tokenizer.get_vocab()), d_model=256, d_ff=1024, num_layers=4, dropout=0.0)
     lit_model = LitTrainer(model, en_tokenizer, hi_tokenizer)
 
+    early_stop_callback = EarlyStopping(monitor="val_accuracy", min_delta=0.00, patience=3, verbose=True, mode="max")
     trainer = L.Trainer(
-        max_epochs=10,
+        max_epochs=50,
         accelerator="auto", # Automatically detects GPU/TPU/MPS
-        devices=1,
+        devices="auto",
+        check_val_every_n_epoch=3,
+        callbacks=[early_stop_callback]
     )
     trainer.fit(model=lit_model, train_dataloaders=train_dataloader, val_dataloaders=validation_dataloader)
+    wandb.finish()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -47,4 +60,6 @@ if __name__ == "__main__":
     
     args_dict = vars(parser.parse_args())
     train_args = TrainArgs(**args_dict) # Pydantic validates here
+
+    wandb.login()
     train(train_args)
